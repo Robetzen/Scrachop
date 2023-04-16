1ec9a09f114bf10aa088afc7d91ab78aaaa5d01c
Robetzen <zarlos11111@gmail.com>
2023-04-16 17:45:29 -0400

    creating home page

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+
+
+app = Flask(__name__)
+
+
+if __name__ == "__main__": 
+  app.run(debug=True)
+ 
+@app.rote('/')  
+def index():
+  page = requests.get('https://www.bbc.com/news')
+  soup = BeautifulSoup(page.content, 'html.parser')
+  headlines = soup.find_all('h3', class_='gs-c-promo-heading__title gel-paragon-bold nw-o-link-split__text')
+  return render_template('index.html', headlines=headlines)
+ 
