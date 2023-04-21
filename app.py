7b7e95fd2c6ca3c319938c977d5b98ca6fff6cc9
angel <perdonholaperdon123@gmail.com>
2023-04-21 04:47:55 -0400

    Adding the form

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-if __name__ == "__main__": 
-  app.run(debug=True)
- 
-@app.route('/')  
+@app.route('/h')  
 def index():
   page = requests.get('https://www.bbc.com/news')
   soup = BeautifulSoup(page.content, 'html.parser')
-  headlines = soup.find_all('h3', class_='gs-c-promo-heading__title gel-paragon-bold nw-o-link-split__text')
+  headlines = soup.find_all('h1', class_='gs-c-promo-heading__title gel-paragon-bold nw-o-link-split__text')
   return render_template('index.html', headlines=headlines)
+ 
+
+if __name__ == "__main__": 
+  app.run(debug=True)
  
